bc5cf58ab5dc5bf9ac8a9f984b5be4c8667c4530
kylestormcloud <58742702+kylestormcloud@users.noreply.github.com>
2020-02-25 10:44:00 -0500

    Update app.py

--- 2-23/app.py
+++ 2-23/app.py
@@ -1,9 +1,14 @@
+# Import the required tools from flask
 from flask import Flask, render_template, request
+
+# Import the Volume class
 from functions import Volume
 
 app = Flask(__name__)
 
-# Instantiate the Document object for Title 13
+# Instantiate the Document object for Title 13, Volume 1
+# The same process could be applied to other volumes.
+# The choice of a single volume simplifies the demostration. 
 CFR = Volume('CFR_Title13_Volume1.xml')
 
 @app.route("/", methods=["GET"])
@@ -13,13 +18,26 @@ def index():
 @app.route("/summary", methods=["POST"])
 def summary():
     
+    # Retrieve the section number input by the user
+    # from the home page.
     number = int(request.form["sectno"])
     print(number)
 
+    # The search_by_number method returns a tuple.
     result = CFR.search_by_number(number)
 
+    # The first element in the tuple "result" is True,
+    # confirming that a section was found.
+    
+    # The second element is the Section object
+    # whose number matches the user's input.
+    
+    # To get the Section object, assign "section" to
+    # the second element of "result" (index 1).
     section = result[1]
     
+    # The summary page will display both the summary
+    # and the original text.
     text = section.text
     summary = section.summary
     
